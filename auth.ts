ed25dda6799559aa3b81cd42e2a262af351104dd
Ziyaa Danil M <ziyaadanil37@gmail.com>
2025-09-30 18:02:02 +0700

    install dan kofigurasi prisma untuk database

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google" 
 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma), 
   providers: [Google],
   session : { strategy: "jwt",},
   pages : {
